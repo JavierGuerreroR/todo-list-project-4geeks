dc62c267a579668340ab95db44a5b9d1c53f2c77
Javier GR <121613893+JavierGuerreroR@users.noreply.github.com>
2023-04-21 19:03:09 +0000

    first contact

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
+
 const Home = () => {
+
+	// Declaración de stocks
+	const [task, addTask]=useState("")
+
+	// function newTask(event)	{
+	// 	addTask(event.target.value);
+	// }
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">To Do List</h1>
+			<input class="form-control form-control-lg" onChange={(event)=>newTask(event.target.value)} type="text" placeholder="New Task" aria-label=".form-control-lg example"></input>
 		</div>
 	);
 };
